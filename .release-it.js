0e96c728f3e3917fd93ecb6acf9fb3ca9969e1a2
dimaslz <me@dimaslz.io>
2021-06-14 11:24:55 +0200

    chore(release): fix release config

--- .release-it.js
+++ .release-it.js
@@ -10,7 +10,7 @@ module.exports = {
 	"hooks": {
     // "before:init": ["npm run lint", "npm test"],
     // "after:my-plugin:bump": "./bin/my-script.sh",
-    "after:bump": "NODE_ENV=production yarn --cwd lib build",
+    "after:bump": "NODE_ENV=production yarn build:lib",
     "after:git:release": "echo After git push, before github release",
     "after:release": "echo Successfully released ${name} v${version} to ${repo.repository}."
   },
