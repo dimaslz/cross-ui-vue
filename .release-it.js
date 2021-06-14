5b6848e77dd8b9cccba2e92a75d220fba6afcd3e
dimaslz <me@dimaslz.io>
2021-06-14 09:46:39 +0200

    chore(release): add release-it chore(env): add commit lint

---
+++ .release-it.js
@@ -0,0 +1,27 @@
+module.exports = {
+  "github": {
+    "push": false
+  },
+	"npm": {
+    "publish": false,
+    "publishPath": "./lib",
+    "access": "public"
+	},
+	"hooks": {
+    // "before:init": ["npm run lint", "npm test"],
+    // "after:my-plugin:bump": "./bin/my-script.sh",
+    "after:bump": "NODE_ENV=production yarn --cwd lib build",
+    "after:git:release": "echo After git push, before github release",
+    "after:release": "echo Successfully released ${name} v${version} to ${repo.repository}."
+  },
+  "git": {
+    "commitMessage": "chore: release v${version}",
+    "requireCleanWorkingDir": false
+  },
+  "plugins": {
+    "@release-it/bumper": {
+      "in": "./lib/package.json",
+      "out": "./lib/package.json"
+    }
+  }
+}
